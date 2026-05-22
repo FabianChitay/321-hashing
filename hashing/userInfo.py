7ff1b6aef4bcf78938d2cb8602ea4a177330c691
Robert-t-t <rcdiaz293@gmail.com>
2026-05-21 23:34:52 -0700

    setup: hasing

---
+++ hashing/userInfo.py
@@ -0,0 +1,13 @@
+
+class UserInfo:
+
+    def __init__(self, user, algorithm, workFactor, salt, hash, userStr):
+        self.user = user
+        self.algorithm = algorithm
+        self.workFactor = workFactor
+        self.salt = salt
+        self.hash = hash
+        self.userStr = userStr
+
+
+
